962523bf4cb45124eef9e27ca50d2430038367b4
Thiago fidelis <139066077+Thiagodevth@users.noreply.github.com>
2024-08-31 23:25:40 -0300

    commit: continuação do menu de seleção de categoria

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import ProductsLogo from '../../assets/products-logo.svg'
-import { Container, ProductsImg } from "./styles";
+import { Container, ProductsImg, CategoryButton, CategoryMenu } from "./styles";
+import api from '../../services/api'
 
 function Products() {
+    const [categories, setCategories] = useState([])
+    const [actveCategory, setActveCategory] = useState(0)
+    useEffect(() => {
+        async function loadCategories() {
+            const { data } = await api.get('categories')
+
+            const newCategories = [{ id: 0, name: 'Todos' }, ...data]
+
+            setCategories(newCategories)
+        }
+
+        loadCategories()
+    }, [])
     return (
         <Container>
-            <ProductsImg src={ProductsLogo} alt="logo-de-products" />
+            <ProductsImg src={ProductsLogo} alt="logo-de-produtos" />
+            <CategoryMenu>
+                {categories && categories.map(category =>
+                    <CategoryButton
+                        type="button"
+                        key={category.id}
+                        isActiveCategory={actveCategory === category.id}
+                        onClick={() => { setActveCategory(category.id) }} >
+                        {category.name}</CategoryButton>
+                )}
+            </CategoryMenu>
         </Container>
     )
 }
--- src/containers/Products/styles.js
+++ src/containers/Products/styles.js
@@ -5,3 +5,20 @@ export const Container = styled.div``
 export const ProductsImg = styled.img`
    width: 100%
 `;
+
+export const CategoryMenu = styled.div`
+   display: flex;
+   justify-content: center;
+   gap: 50px;
+   margin-top: 20px;
+`;
+
+export const CategoryButton = styled.button`
+   cursor: pointer;
+   background: none;
+   border: none;
+   border-bottom: ${props => (props.isActiveCategory && '3px solid #9758A6')};;
+   color: ${props => (props.isActiveCategory ? '#9758A6' : '#9a9a9d')};
+   font-size: 17px;
+   line-height: 20px;
+`;
