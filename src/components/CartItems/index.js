04fe508ce1f8af830dc366848c23f7c6a75e5ada
Thiago fidelis <139066077+Thiagodevth@users.noreply.github.com>
2024-09-04 23:10:59 -0300

    commit: Criando a lógica do carrinho, criando estrutura do carrinho e estilizando o carrinho. estilizando botões de quantidade

--- src/components/CartItems/index.js
+++ src/components/CartItems/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useCart } from '../../hooks/CartContext'
-import { Container, Header, Body } from "./styles";
+import { Container, Header, Body, EmptyCart } from "./styles";
 import formatCurrency from "../../utils/formatCurrency";
 
 export function CartItems() {
@@ -13,19 +13,29 @@ export function CartItems() {
                 <p></p>
                 <p>Itens</p>
                 <p>Preço</p>
-                <p>Quantidade</p>
+                <p style={{ paddingRight: 40 }}>Quantidade</p>
                 <p>Total</p>
             </Header>
 
-            {cartProducts && cartProducts.map(product => (
-                <Body key={product.id}>
-                    <img src={product.url} alt="imagem do produto" />
-                    <p>{product.name}</p>
-                    <p>{formatCurrency(product.price)}</p>
-                    <p>{product.quantity}</p>
-                    <p>{formatCurrency(product.quantity * product.price)}</p>
-                </Body>
-            ))}
+            {cartProducts && cartProducts.length > 0 ?
+                cartProducts.map(product => (
+                    <Body key={product.id}>
+                        <img src={product.url} alt="imagem do produto" />
+                        <p>{product.name}</p>
+                        <p>{formatCurrency(product.price)}</p>
+                        <div className="quantity-container">
+                            <button>-</button>
+                            <p>{product.quantity}</p>
+                            <button>+</button>
+                        </div>
+
+                        <p>{formatCurrency(product.quantity * product.price)}</p>
+                    </Body>
+                ))
+                : (
+                    <EmptyCart>Carrinho vazio</EmptyCart>
+                )
+            }
 
         </Container>
     )
--- src/components/CartItems/styles.js
+++ src/components/CartItems/styles.js
@@ -1,9 +1,61 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-   
+   background-color: #ffffff;
+   box-shadow: 10px 40px rgba(0, 0, 0, 0.03);
+   border-radius: 20px;
+   padding: 10px;
+   width: max-content;
    `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+   display: grid;
+   grid-template-columns: repeat(5,1fr);
+   padding: 10px;
+   border-bottom: 1px solid #b5b5b5;
+   p{
+      font-size: 16px;
+      color: #b5b5b5;
+   }
+   ;`
 
-export const Body = styled.div``;
+export const Body = styled.div`
+   display: grid;
+   grid-template-columns: repeat(5,1fr);
+   width: max-content;
+   gap: 10px 15px;
+   padding: 10px;
+   align-items: center;
+
+   img{
+      border-radius: 10px;
+      width: 120px;
+   }
+
+   p{
+      font-size: 16px;
+      color: #000000;
+   }
+
+   .quantity-container{
+      display: flex;
+      gap: 20px;
+      align-items: center;
+
+      button{
+         height: 30px;
+         background: transparent;
+         border: none;
+         cursor: pointer;
+
+      }
+      p{
+         margin-top: 0px;
+      }
+   }
+   `;
+
+export const EmptyCart = styled.p`
+   padding: 20px;
+   text-align: center;
+   `;
