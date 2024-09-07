873b5efc01f7d9a464cc74f8c652a44c055a07f3
Thiago fidelis <139066077+Thiagodevth@users.noreply.github.com>
2024-09-06 23:47:40 -0300

    commit: estilização do container resumo do carrinho

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 import { Container } from "./styles";
+import { Button } from '../Button'
 
 export function CartResume() {
     return (
-        <Container>
+        <>
+            <Container>
+                <div className="container-top">
+                    <h2 className="title">Resumo do pedido</h2>
+                    <p className="items">Itens</p>
+                    <p className="items-price">R$ 10,00</p>
+                    <p className="delivery-tax">Taxa de entrega</p>
+                    <p className="delivery-tax-price">R$ 7,00</p>
+                </div>
+                <div className="container-bottom">
+                    <p>Total</p>
+                    <p>R$ 49,00</p>
+                </div>
+                <Button style={{ width: '100%' }}>
+                    Finalizar pedido
+                </Button>
+            </Container>
 
-        </Container>
+        </>
     )
 }
 
--- src/components/CartResume/styles.js
+++ src/components/CartResume/styles.js
@@ -1,6 +1,52 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-   background: #e5e5e5;
-   min-height: 100vh;
-   `;
+   background-color: #ffffff;
+   min-height: 33vh;
+   box-shadow: 10px 10px rgba(0, 0, 0, 0.03);
+   border-radius: 20px;
+   padding: 15px;
+   width: 25%;
+   margin-bottom: 40px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+
+   .container-top {
+      display: grid;
+      grid-template-areas:
+      'title title'
+      'items items-price'
+      'delivery-tax delivery-tax-price';
+
+      .title {
+      grid-area: title;
+      margin-bottom: 20px;
+      }
+      .items {
+      grid-area: items;
+      }
+      .items-price {
+      grid-area: items-price;
+      }
+      .delivery-tax {
+      grid-area: delivery-tax;
+      }
+      .delivery-tax-price {
+      grid-area: delivery-tax-price;
+      }
+
+   }
+
+ 
+   .container-bottom{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      font-size: 24px;
+   }
+
+`;
+
+export const Button = styled.button`
+   width: 100%;`
