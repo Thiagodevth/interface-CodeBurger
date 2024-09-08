37036085fbbbcbceecd6786b8b8a7ca3cf0e7042
Thiago fidelis <139066077+Thiagodevth@users.noreply.github.com>
2024-09-07 23:30:15 -0300

    commit: continuação da logica do resumo do carrinho

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container } from "./styles";
 import { Button } from '../Button'
+import formatCurrency from '../../utils/formatCurrency'
+import { useCart } from '../../hooks/CartContext'
 
 export function CartResume() {
-    return (
-        <>
-            <Container>
-                <div className="container-top">
-                    <h2 className="title">Resumo do pedido</h2>
-                    <p className="items">Itens</p>
-                    <p className="items-price">R$ 10,00</p>
-                    <p className="delivery-tax">Taxa de entrega</p>
-                    <p className="delivery-tax-price">R$ 7,00</p>
-                </div>
-                <div className="container-bottom">
-                    <p>Total</p>
-                    <p>R$ 49,00</p>
-                </div>
-                <Button style={{ width: '100%' }}>
-                    Finalizar pedido
-                </Button>
-            </Container>
+  const [finalPrice, setFinalPrice] = useState(0)
+  const [deliveryTax] = useState(5)
 
-        </>
-    )
+  const { cartProducts } = useCart()
+
+  useEffect(() => {
+    const sumAlltems = cartProducts.reduce((acc, current) => {
+      return current.price * current.quantity + acc
+    }, 0)
+
+    setFinalPrice(formatCurrency(sumAlltems + deliveryTax))
+  }, [cartProducts, deliveryTax])
+
+  return (
+    <>
+      <Container>
+        <div className="container-top">
+          <h2 className="title">Resumo do pedido</h2>
+          <p className="items">Itens</p>
+          <p className="items-price">{finalPrice}</p>
+          <p className="delivery-tax">Taxa de entrega</p>
+          <p className="delivery-tax-price">{formatCurrency(deliveryTax)}</p>
+        </div>
+        <div className="container-bottom">
+          <p>Total</p>
+          <p>{formatCurrency(finalPrice + deliveryTax)}</p>
+        </div>
+        <Button style={{ width: '100%' }}>
+          Finalizar pedido
+        </Button>
+      </Container>
+
+    </>
+  )
 }
 
