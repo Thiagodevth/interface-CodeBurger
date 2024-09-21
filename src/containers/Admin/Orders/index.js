ab71f9583b494bb90ff9d731bdf80532e4a1f6b5
Thiago fidelis <139066077+Thiagodevth@users.noreply.github.com>
2024-09-20 23:59:19 -0300

    commit: Arrumando nossa tabela

--- src/containers/Admin/Orders/index.js
+++ src/containers/Admin/Orders/index.js
@@ -1,31 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 import api from '../../../services/api'
 import { Container } from './styles'
+import Row from "./row";
 
 function Orders() {
   const [orders, setOrders] = useState([])
   const [rows, setRows] = useState([])
+
   console.log(orders)
+
   useEffect(() => {
     async function loadOrders() {
       const { data } = await api.get('orders')
-
 
       setOrders(data)
     }
@@ -50,7 +45,24 @@ function Orders() {
   console.log(rows)
   return (
     <Container>
-
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Pedidos</TableCell>
+              <TableCell >Cliente</TableCell>
+              <TableCell >Data do pedido</TableCell>
+              <TableCell >Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <Row key={row.orderId} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   )
 }
